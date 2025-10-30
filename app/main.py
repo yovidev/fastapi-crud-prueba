450bbe522efe68ebe81e0ae6025526c7f64201fe
Yovanis Suárez <yovanis.suarez.s@gmail.com>
2025-10-30 13:09:27 -0400

    chore: initial project structure, postgres config and deps

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-from .router import auth, users, posts, tags
+from .routers import auth, users, posts, tags
 from .core.database import engine
 from .models.entities import Base
 
 app = FastAPI(title="crud_fastapi - prueba tecnica", version="1.0.0")
 
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
